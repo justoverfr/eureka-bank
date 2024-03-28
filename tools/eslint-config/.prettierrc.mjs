06e1c53945691b70bfc604c4a14d01f626e02ab3
justoverfr <jeanjuste.lefebvre@gmail.com>
2024-03-28 12:30:04 +0100

    refactor(eslint-config): move prettier config into package.json

--- tools/eslint-config/.prettierrc.mjs
+++
@@ -1,8 +0,0 @@
-import prettierConfig from '@eureka-bank/prettier-config';
-
-/** @typedef  {import("prettier").Config} PrettierConfig */
-
-/** @type { PrettierConfig } */
-export default {
-  ...prettierConfig,
-};
