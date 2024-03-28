b86d80ff75cfc13ec6be4dfe1c31dab7a2ccefc9
justoverfr <jeanjuste.lefebvre@gmail.com>
2024-03-28 12:18:39 +0100

    refactor(prettier): move imports to top of config

--- tools/eslint-config/.prettierrc.mjs
+++ tools/eslint-config/.prettierrc.mjs
@@ -1,9 +1,8 @@
+import prettierConfig from '@eureka-bank/prettier-config';
+
 /** @typedef  {import("prettier").Config} PrettierConfig */
 
 /** @type { PrettierConfig } */
-
-import prettierConfig from '@eureka-bank/prettier-config';
-
 export default {
   ...prettierConfig,
 };
