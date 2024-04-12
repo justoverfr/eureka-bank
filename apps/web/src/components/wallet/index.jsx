fa7291970e4fbf561f827198a16e320a63f97cc8
justoverfr <jeanjuste.lefebvre@gmail.com>
2024-04-12 12:58:15 +0200

    feat(web): logout button

--- apps/web/src/components/wallet/index.jsx
+++ apps/web/src/components/wallet/index.jsx
@@ -1,4 +1,5 @@
 import Image from 'next/image';
+import Link from 'next/link';
 
 import { getServerSession } from 'next-auth';
 
@@ -24,99 +25,12 @@ async function Wallet() {
           <h3 className="text-bgray-900 text-lg font-bold dark:text-white">My Wallet</h3>
           <PaymentSelect />
         </div>
-
-        <div className="text-bgray-900 break-words text-lg font-bold dark:text-white">
-          {session && session.user.walletAddress}
-          {/* <div className="card-slider relative w-[280px] md:w-[340px]">
-            <Slider
-              slidesPerView={1}
-              loop={true}
-              effect={'fade'}
-              autoplay={{
-                delay: 2500,
-                disableOnInteraction: false,
-              }}
-            >
-              <div className="w-full">
-                <Image
-                  priority={true}
-                  height={card1.height}
-                  width={card1.width}
-                  src={card1.src}
-                  alt="card"
-                />
-              </div>
-              <div className="w-full">
-                <Image
-                  priority={true}
-                  height={card2.height}
-                  width={card2.width}
-                  src={card2.src}
-                  alt="card"
-                />
-              </div>
-              <div className="w-full">
-                <Image
-                  priority={true}
-                  height={card3.height}
-                  width={card3.width}
-                  src={card3.src}
-                  alt="card"
-                />
-              </div>
-            </Slider>
-          </div> */}
-        </div>
+        <Link href={`https://sepolia.etherscan.io/address/${session?.user.walletAddress}`}>
+          <div className="text-bgray-900 break-words text-lg font-bold underline dark:text-white">
+            {session && session.user.walletAddress}
+          </div>
+        </Link>
       </div>
-      {/* <div className="w-full">
-        <h3 className="text-bgray-900 mb-4 text-lg font-bold dark:text-white">Quick Transfer</h3>
-        <PaymentFilter />
-        <div className="border-bgray-200 focus-within:border-success-300 dark:border-darkblack-400 flex h-[98px] w-full flex-col justify-between rounded-lg border p-4">
-          <p className="text-bgray-600 dark:text-bgray-50 text-sm font-medium">
-            Enter recipient's wallet address
-          </p>
-          <div className="flex h-[35px] w-full items-center justify-between">
-            <label className="w-full">
-              <input
-                type="text"
-                className="text-bgray-900 dark:border-darkblack-400 dark:bg-darkblack-600 w-full border-none p-0 text-2xl font-bold focus:outline-none focus:ring-0 dark:text-white"
-              />
-            </label>
-            <div>
-              <Image
-                priority={true}
-                height={member.height}
-                width={member.width}
-                src={member.src}
-                alt="members"
-              />
-            </div>
-          </div>
-        </div>
-        <br />
-        <div className="border-bgray-200 focus-within:border-success-300 dark:border-darkblack-400 flex h-[98px] w-full flex-col justify-between rounded-lg border p-4">
-          <p className="text-bgray-600 dark:text-bgray-50 text-sm font-medium">Enter amount</p>
-          <div className="flex h-[35px] w-full items-center justify-between">
-            <span className="text-bgray-900 font-bold dark:text-white">ERFB </span>
-            <label className="w-full">
-              <input
-                type="text"
-                className="text-bgray-900 dark:border-darkblack-400 dark:bg-darkblack-600 w-full border-none p-0 text-2xl font-bold focus:outline-none focus:ring-0 dark:text-white"
-              />
-            </label>
-            <div>
-              <Image
-                priority={true}
-                height={member.height}
-                width={member.width}
-                src={member.src}
-                alt="members"
-              />
-            </div>
-          </div>
-        </div>
-        <GreenBtn type="submit" text="Send Money" className="mt-7" />
-      </div> */}
       <QuickTransfer session={session} />
     </div>
   );
